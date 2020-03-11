714eb041d8bfe832b84371506639ce0df0fc94ad
EHGP <43518665+ehgp@users.noreply.github.com>
2020-03-10 22:52:26 -0400

    New Description for API Batch Call

--- APICALLBATCH.py
+++ APICALLBATCH.py
@@ -1,3 +1,4 @@
+#using responses, cURL and snakebite to access Hadoop Windows Instance VM and put files into HDFS folder.
 import os
 import requests
 import json
@@ -11,6 +12,7 @@ headers = {"Authorization":"Bearer xxxxxx", "Content-Type": "application/json"}
 data = '{"query":"(AI OR Artificial Intelligence OR Machine Learning)", "fromDate": "201602020000", "toDate": "201902240000" , "maxResults":10}'
 response = requests.post(endpoint,data=data,headers=headers).json()
 file = json.dumps(response, indent = 2)
+# put downloaded data into local disk temporarily
 with open('data.txt', 'w') as outfile:
     json.dump(file, outfile)
 #for p in client1.mkdir(['/twitter_data_hist']):
